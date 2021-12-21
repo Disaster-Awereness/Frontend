ea9175441d98445c4fc597a7c0e6dd5cc3a2b482
greymouse1 <68509154+greymouse1@users.noreply.github.com>
2021-12-21 18:19:41 +0100

    Remove about part

--- disasters-site.nosync/src/app/app.component.ts
+++ disasters-site.nosync/src/app/app.component.ts
@@ -4,26 +4,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  
-  <app-navbar></app-navbar>
-
-  <!--Header-->
+    <!--Header-->
   <app-header></app-header>
 
   <!--Routes injected here!-->
   <router-outlet></router-outlet>
 
-
   <app-banner></app-banner>
 
   <app-introduction></app-introduction>
 
-  <!--Map section below -->
-
-
   <app-cards></app-cards>
-
-
 
   <!--Footer-->
   <app-footer></app-footer>
