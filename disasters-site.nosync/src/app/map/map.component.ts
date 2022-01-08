a483b7eeb8eb8b135dd238ff3a58ea187ddce877
Ben Buchenau <ben.buchenau@bicc.de>
2022-01-08 17:49:50 +0100

    added plate tectonics

--- disasters-site.nosync/src/app/map/map.component.ts
+++ disasters-site.nosync/src/app/map/map.component.ts
@@ -1,6 +1,7 @@
 import { Component, AfterViewInit } from '@angular/core';
 import * as L from 'leaflet';
 import 'mapbox-gl-leaflet';
+import * as $ from 'jquery/dist/jquery.min.js';
 import { MarkerService } from '../marker.service';
 
 @Component({
@@ -24,16 +25,27 @@ export class MapComponent implements AfterViewInit {
     //Set tile options for basemap.
     const tiles = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYmVuYnVjaGVuYXUiLCJhIjoiY2tvdHRoZDJ0MGVybTJybGxtcHQwMXViZSJ9.JaW_435Cnt_dk1WXzFzajg', {
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-      maxZoom: 10,
-      minZoom: 1,
+      maxZoom: 9,
+      minZoom: 2,
       id: 'mapbox/light-v10',
       accessToken: 'pk.eyJ1IjoiYmVuYnVjaGVuYXUiLCJhIjoiY2tvdHRoZDJ0MGVybTJybGxtcHQwMXViZSJ9.JaW_435Cnt_dk1WXzFzajg'
     });
 
     tiles.addTo(this.map);
 
-    const legend = new L.Control({position: 'bottomleft'});
+    //Load plate tectonics GeoJSON from Github.
+    $.getJSON("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json", (response) => {
+      console.log("response", response);
+      var tectonicPlates = new L.GeoJSON(response);
+      tectonicPlates.setStyle({
+        color: '#93b6c9',
+        opacity: .8
+      });
+      tectonicPlates.addTo(this.map) 
+    });   
 
+    //Add legend...
+    const legend = new L.Control({position: 'bottomleft'});
     legend.onAdd = function (map) {
       var div = L.DomUtil.create('div', 'info legend'),
       grades= [0, 100],
