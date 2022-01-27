a8bd9d7b601462bc5987a2d25c7cb734433ddab9
ahmad-fahim <fahim.univdhaka@gmail.com>
2022-01-27 13:00:38 +0600

    Adjust space between paragraphs in consequences section

--- disasters-site.nosync/src/app/consequences/consequences.component.ts
+++ disasters-site.nosync/src/app/consequences/consequences.component.ts
@@ -20,15 +20,15 @@ import { Component, OnInit } from '@angular/core';
         </ul> 
         <p>
           <b>Ground Shaking</b> is a term used to describe the vibration of the ground during an earthquake. It is the most familiar effect of earthquakes. It is a result of the passage of seismic waves through the ground, and ranges from quite gentle in small earthquakes to incredibly violent in large earthquakes.
-          <br>
+           
           <b>Ground Rupture</b> is another important effect of earthquakes which occurs when the earthquake movement along a fault actually breaks the Earth's surface. Rupture causes problems for humans by, well, rupturing things; pipelines, tunnels, aqueducts, railway lines, roads, and airport runways which cross an area of active rupture can easily be destroyed or severely damaged. 
-          <br>
+          
           <b>Landslides</b> are caused by earthquakes both by direct rupture and by sustained shaking of unstable slopes. They can easily destroy buildings in their path, or block roads and railroad lines, or take hilltop homes with them as they tumble.
-          <br>
+           
           <b>Tsunamis</b> , which are popularly -- and incorrectly -- known as "tidal waves", are a grave hazard to many parts of the world, particularly around the Pacific Ocean basin. Tsunamis are a series of water waves caused when the seafloor moves vertically in an earthquake and which can travel vast distances in a short period of time. Tsunami speeds in the deep ocean have been measured at more than 700 km/hr, comparable to some jet planes, and when tsunamis reach shallow water near the coast, they can reach heights of more than 27 meters (90 feet)!
-          <br>
+           
           <b>Liquefaction</b>  and subsidence of the ground are important effects which often are the cause of much destruction in earthquakes, particularly in unconsolidated ground. Liquefaction is when sediment grains are literally made to float in groundwater, which causes the soil to lose all its solidity. Subsidence can then follow as the soil recompacts. Sand blows, or sand volcanoes, form when pressurized jets of groundwater break through the surface. They can spray mud and sand over an area a few meters across. All of these effects pose a grave danger to buildings, roads, train lines, airport runways, gas lines, etc.
-          <br>
+          
           <b>Fires</b> are a major source of damage after earthquakes. Ground rupture and liquefaction can easily rupture natural gas mains and water mains, both contributing to the ignition of fires and hindering the efforts to control them.
 
         </p>
